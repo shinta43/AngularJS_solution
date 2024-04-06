341856392d4dc9fc8da42d568627299a063bdc33
shynta2023@gmail.com <shynta2023@gmail.com>
2024-04-06 17:57:25 +0800

    add index.html, css, fonts, images, lib and etc

---
+++ module5-solution/src/common/common.module.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  angular.module("common", []).constant("ApiPath", "https://aleksandar-gjorgievski.herokuapp.com").config(config);
+
+  config.$inject = ["$httpProvider"];
+  function config($httpProvider) {
+    $httpProvider.interceptors.push("loadingHttpInterceptor");
+  }
+})();
---
+++ module5-solution/src/public/public.module.js
@@ -0,0 +1,7 @@
+(function () {
+  "use strict";
+  /**
+   * Public restaurant application. Includes the common module and ui-router.
+   */
+  angular.module("public", ["ui.router", "common"]);
+})();
